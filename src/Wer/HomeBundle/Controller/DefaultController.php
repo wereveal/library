190335311216bb696a364c6b11b13abd13a63b8f
William E Reveal (from Mac) <wer@wereveal.com>
2013-03-14 14:32:32 -0500

    Added Bootstrap for view framework. Modified ::base.html.twig
    Ready to start on importing new Sobi data and work on the SobiBundle

--- src/Wer/HomeBundle/Controller/DefaultController.php
+++ src/Wer/HomeBundle/Controller/DefaultController.php
@@ -6,8 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('WerHomeBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render(
+            'WerHomeBundle:Default:index.html.twig',
+            array(
+                'title'       => 'Hello!',
+                'description' => '',
+                'site_url'    => "http://{$_SERVER['SERVER_NAME']}",
+                'body_text'   => 'Hello!'
+            )
+        );
     }
 }
