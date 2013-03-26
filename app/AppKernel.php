1e5a86bbed33ba00db4a86989d641a586b0c54e6
William E Reveal (from Mac) <wer@wereveal.com>
2013-03-26 14:36:59 -0500

    Getting rid of the Acme bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,12 +20,10 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Wer\GuideBundle\WerGuideBundle(),
-            new Wer\SobiBundle\WerSobiBundle(),
-            new Wer\HomeBundle\WerHomeBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
+            // $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
