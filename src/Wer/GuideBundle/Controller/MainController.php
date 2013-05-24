f4ef44e862cf660eddbe191be61f7633ac0cc5e6
William E Reveal (from Mac) <wer@wereveal.com>
2013-05-24 17:50:51 -0500

    Working on merging work from mxg for the main controller

--- src/Wer/GuideBundle/Controller/MainController.php
+++ src/Wer/GuideBundle/Controller/MainController.php
@@ -10,11 +10,23 @@ class MainController extends Controller
     private $default_section = 1;
     private $num_to_display  = 10;
     private $o_item;
+    protected $o_arr;
+    protected $o_cat;
+    protected $o_elog;
+    protected $o_item;
+    protected $o_sec;
+    protected $o_shared_view;
+    protected $o_view;
 
     public function __construct()
     {
+        $this->o_arr  = new Arrays();
+        $this->o_cat  = new Category();
+        $this->o_elog = Elog::start();
         $this->o_item = new Item();
-    }
+        $this->o_sec  = new Section();
+        $this->o_shared_view = new Shared();
+   }
     public function indexAction()
     {
         /*  What are we doing?
@@ -37,7 +49,7 @@ class MainController extends Controller
         $a_twig_values = array(
             'title'         => 'Guide',
             'description'   => 'This is a description',
-            'site_url'      => "http://{$_SERVER['SERVER_NAME']}",
+            'site_url'      => SITE_URL,
             'rights_holder' => 'William E. Reveal',
             'quick_form'    => $a_quick_form,
             'alpha_list'    => $a_alpha_list,
@@ -75,28 +87,33 @@ class MainController extends Controller
     public function alphaList($current_letter = '')
     {
         $main_str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-        $a_li_values = array();
-        $first_row = true;
+        $a_li_values  = array();
+        $first_letter = $main_str[0];
+        $last_letter  = $main_str[count($main_str) - 1];
         for ($i = 0; $i < strlen($main_str); $i++) {
             $the_letter = $main_str[$i];
+            $the_class  = 'otherLetter';
+            $is_link    = true;
             $results = $this->o_item->readItemByNameFirstLetter($the_letter);
-            $a_li_row = array(
-                'is_link' => true,
-                'the_class' => 'otherLetter',
-                'the_letter' => $the_letter
-            );
             if ($results != false && count($results) > 0) {
-                if ($first_row) {
-                    $a_li_row['the_class'] = 'firstLetter';
-                    $a_li_row['the_letter'] = $the_letter;
+                if ($the_letter == $first_letter) {
+                    $the_class = 'firstLetter';
+                } elseif ($the_letter == $last_letter) {
+                    $the_class = 'lastLetter';
                 }
                 if ($the_letter == $current_letter) {
-                    $a_li_row['the_class'] .= ' currentLetter';
-                    $a_li_row['is_link'] = false;
+                    $the_class .= ' currentLetter';
+                    $is_link = false;
                 }
             } else {
-                $a_li_row['is_link'] = false;
+                $the_class = 'noLink';
+                $is_link   = false;
             }
+            $a_li_row = array(
+                'the_letter' => $the_letter
+                'the_class'  => $the_class,
+                'is_link'    => $is_link,
+            );
             $a_li_values[$i] = $a_li_row;
         }
         return $a_li_values;
@@ -136,12 +153,39 @@ class MainController extends Controller
     }
     /**
      *  creates the values needed for the section list
-     *  @param int $section_id
-     *  @return array $a_values
+     *  @param mixed $selected_section can be an id or a name
+     *  @param array $a_search_parameters
+     *      (see o_sec->readSection comments for more info)
+     *  @return array $a_return_this
     **/
-    public function sectionList($section_id = 1)
+    public function sectionList($selected_section = '', $a_search_parameters = '')
     {
-        return array();
+        $a_return_this = array();
+        $a_sections = $this->o_sec->readSection('', $a_search_parameters);
+        if (count($a_sections) > 1) {
+            foreach ($a_sections as $a_section) {
+                if ($selected_section == '') {
+                    $selected_section = $a_section['sec_id'];
+                }
+                $a_return_this['options'][] = array(
+                    'o_value'     => $a_section['sec_id'],
+                    'o_label'     => $a_section['sec_id'],
+                    'other_stuph' => $selected_section == $a_section['sec_id'] ? ' selected' : '';
+                );
+            }
+            $a_return_this['select_values'] = array(
+                'select_name' => 'sectionSelect',
+                'class'       => 'sectionSelect',
+                'other_stuph' => ' id="sectionSelect" onchange="searchBySection(this)"'
+            );
+            $a_return_this['label_values'] = array(
+                'for'        => 'sectionSelect',
+                'label_text' => 'Search By Section',
+                'class'      => 'sectionSelectLabel'
+            );
+        }
+        $this->o_elog->write("select list array\n" . var_export($a_return_this, true), LOG_OFF, __METHOD__ . '.' . __LINE__);
+        return $a_return_this;
     }
     ### Utilities ###
 
