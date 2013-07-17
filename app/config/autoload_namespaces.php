724976ddb374bf8c7a01988a65000ea28049debc
William E Reveal (from Mac) <wer@wereveal.com>
2013-07-17 17:41:45 -0500

    Making the framework standalone

--- app/config/autoload_namespaces.php
+++ app/config/autoload_namespaces.php
@@ -2,11 +2,4 @@
 
 return array(
     'Wer\\Framework\\Library'   => APP_PATH,
-    'Wer\\Guide\\Controller'    => APP_PATH,
-    'Wer\\Guide\\Model'         => APP_PATH,
-    'Wer\\Guide\\Model\\Entity' => APP_PATH,
-    'Wer\\Guide\\Tests'         => APP_PATH,
-    'Wer\\Guide\\Tests\\Tester' => APP_PATH,
-    'Wer\\Guide\\View'          => APP_PATH,
-    'Wer\\Import\\Controller'   => APP_PATH,
 );
--- app/config/db_config.php
+++ app/config/db_config.php
@@ -1,11 +1,11 @@
 <?php
 return array(
-    'driver'   => 'mysql',
+    'driver'   => 'pgsql',
     'host'     => 'localhost',
     'port'     => '3306',
-    'name'     => 'qcdg_wer',
-    'user'     => 'qcdg',
-    'password' => 'letFoodIn',
-    'userro'   => 'qcdg',
-    'passro'   => 'letFoodIn'
+    'name'     => 'REPLACE_ME',
+    'user'     => 'REPLACE_ME',
+    'password' => 'REPLACE_ME',
+    'userro'   => 'REPLACE_ME',
+    'passro'   => 'REPLACE_ME'
 );
