824aac7e5e5f24bded5bb98a77a6cf083d19cc48
William E Reveal (mb) <bill@revealitconsulting.com>
2014-01-17 16:04:32 -0600

    Added Entity Interface Example

---
+++ Interfaces/EntityInterface.php
@@ -0,0 +1,17 @@
+<?php
+namespace Ritc\FtpAdmin\Models;
+
+interface EntityInterface
+{
+    /**
+     *  returns an array of the properties
+     *  @return array
+    **/
+    public function getAllProperties();
+    /**
+     *  Sets the values of all the entity properties.
+     *  @param array $a_entity e.g., array('id'=>'', 'password'=>'', 'uid'=>'', 'gid'=>'', 'dir'=>'')
+     *  @return bool success or failure
+    **/
+    public function setAllProperties(array $a_entity = array());
+}
