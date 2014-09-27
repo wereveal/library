cb1ea0807021701a5038e986fa9816c58b2a1ba7
William E Reveal (mb) <bill@revealitconsulting.com>
2014-09-26 22:51:43 -0500

    More router stuff. But I keep wondering if this will work.

---
+++ Entities/RouterEntity.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ *  CREATE TABLE `dbPrefix_router` (
+ *    `router_id` int(11) NOT NULL AUTO_INCREMENT,
+ *    `uri_path` varchar(255) NOT NULL,
+ *    `controller` varchar(128) NOT NULL,
+ *    `method` varchar(64) DEFAULT NULL,
+ *    `action` varchar(255) NOT NULL
+ *    PRIMARY KEY (`router_id`),
+ *    UNIQUE KEY `uri_path` (`uri_path``)
+ *  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ */
+
