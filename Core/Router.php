5ae7903fe8ac3e61979b3332150997c282a919b3
William E Reveal (from Mac) <bill@revealitconsulting.com>
2014-10-31 13:11:25 -0500

    Made some after reflection changes, time to test

--- Core/Router.php
+++ Core/Router.php
@@ -21,40 +21,104 @@ class Router extends Base
 {
     protected $o_elog;
     protected $private_properties;
+    private $a_args;
     private $o_db;
-    private $uri;
+    private $uri_path;
 
     public function __construct(DbModel $o_db)
     {
         $this->setPrivateProperties();
-        $this->uri = removeUriArgs();
+        $this->setUriPath();
+        $this->setArgs();
         $this->o_db = $o_db;
     }
 
-    public function removeUriArgs($request_uri = '')
+    /**
+     *  Returns the action parts from the database.
+     *  @param atring $uri_path optional, defaults to class property $uri
+     *  @return array
+     */
+    public function action($uri_path = '')
+    {
+        $db_prefix = $this->o_db->getDbPrefix();
+        if ($uri_path == '') {
+            $uri_path = $this->uriPath;
+        }
+        $a_uri = [':uri_path' => $uri_path];
+        $sql = "
+            SELECT controller, method, action
+            FROM {$db_prefix}routes
+            WHERE uri_path LIKE :uri_path";
+        $a_results = $this->o_db->search($sql, $a_uri);
+        if ($a_results !== false && count($a_results) > 0) {
+            $a_return_this = $a_results[0];
+            $a_return_this['args'] = $this->a_args;
+            return $a_return_this;
+        }
+        else {
+            return [
+                'controller' => 'Main',
+                'method'     => '',
+                'action'     => '',
+                'args'       => array()
+            ];
+        }
+    }
+
+    ### GETters and SETters ###
+    public function getArgs()
+    {
+        return $this->a_args;
+    }
+    public function getUriPath()
+    {
+        return $this->uri_path;
+    }
+    /**
+     *  Sets the property $a_args.
+     *  By entering a string without a '?' it will set the property to an
+     *      empty array.
+     *  @param string $request_uri optional, defaults to $_SERVER['REQUEST_URI']
+     *  @return null
+     */
+    public function setArgs($request_uri = '')
     {
         if ($request_uri == '') {
             $request_uri = $_SERVER["REQUEST_URI"];
         }
         if (strpos($request_uri, "?") !== false) {
-            return substr($request_uri, 0, strpos($request_uri, "?"));
+            $args = substr($request_uri, strpos($request_uri, "?"));
+            $a_arg_pairs = explode("&", $args);
+            $a_final_args = array();
+            foreach ($a_arg_pairs as $arg_pair) {
+                $a_final_args[] = explode("=", $arg_pair);
+            }
         }
         else {
-            return $request_uri;
+            $this->args = array();
+        }
+    }
+    /**
+     *  Sets the property $uri_path.
+     *  @param string $request_uri optional, defaults to $_SERVER['REQUEST_URI']
+     *  @return null
+     */
+    public function setUriPath($request_uri = '')
+    {
+        if ($request_uri == '') {
+            $request_uri = $_SERVER["REQUEST_URI"];
+        }
+        if (strpos($request_uri, "?") !== false) {
+            $this->uri = substr($request_uri, 0, strpos($request_uri, "?"));
+        }
+        else {
+            $this->uri_path = $request_uri;
         }
     }
 
-    public function getRoute()
-    {
-        $db_prefix = $this->o_db->getDbPrefix();
-        $a_uri = [':uri_path' => $this->uri];
-        $sql = "SELECT controller, method, action FROM {$db_prefix}routes WHERE uri_path LIKE :uri_path";
-        $a_results = $this->o_db->search($sql, $a_uri);
-        if ($a_results !== false && count($a_results) > 0) {
-            return $a_results[0];
-        }
-        else {
-            return ['controller' => 'Main', 'method' => '', 'action' => ''];
-        }
-    }
+    /* From Base Abstract
+        protected function getElog
+        protected function logIt
+        protected function setElog
+    */
 }
