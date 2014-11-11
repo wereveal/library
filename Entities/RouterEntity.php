51eacaa2d1681f88101bb025b6e830a5c172b43b
William E Reveal (from Mac) <bill@revealitconsulting.com>
2014-11-11 14:09:55 -0600

    Filled out the RouterEntity

--- Entities/RouterEntity.php
+++ Entities/RouterEntity.php
@@ -1,14 +1,39 @@
 <?php
+/**
+ *  SQL
+ *  MySQL sql
+    CREATE TABLE `dbPrefix_router` (
+      `router_id` int(11) NOT NULL AUTO_INCREMENT,
+      `route_path` varchar(255) NOT NULL,
+      `route_class` varchar(128) NOT NULL,
+      `route_method` varchar(64) DEFAULT NULL,
+      `route_action` varchar(255) NOT NULL
+      PRIMARY KEY (`router_id`),
+      UNIQUE KEY `uri_path` (`uri_path``)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+**/
 
-/*
- *  CREATE TABLE `dbPrefix_router` (
- *    `router_id` int(11) NOT NULL AUTO_INCREMENT,
- *    `route_path` varchar(255) NOT NULL,
- *    `route_class` varchar(128) NOT NULL,
- *    `route_method` varchar(64) DEFAULT NULL,
- *    `route_action` varchar(255) NOT NULL
- *    PRIMARY KEY (`router_id`),
- *    UNIQUE KEY `uri_path` (`uri_path``)
- *  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
- */
+namespace Ritc\Library\Entities;
 
+use Ritc\Library\Interfaces\EntityInterface;
+
+class RouterEntity implements EntityInterface
+{
+    /**
+     * Gets all the entity properties.
+     * @return array
+     */
+    public function getAllProperties()
+    {
+        return array();
+    }
+    /**
+     * Sets all the properties for the entity in one step.
+     * @param array $a_entity
+     * @return bool
+     */
+    public function setAllProperties(array $a_entity = array())
+    {
+        return true;
+    }
+}
