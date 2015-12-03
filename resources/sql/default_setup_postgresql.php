c52ca11104d97653dc82b7f4022cbe1212503100
William E Reveal (from Mac) <bill@revealitconsulting.com>
2015-12-03 15:19:21 -0600

    Changes/addition to postgresql setup

--- resources/sql/default_setup_postgresql.php
+++ resources/sql/default_setup_postgresql.php
@@ -1,36 +1,23 @@
--- Postgresql setup for the Library Framework
--- Replace {$dbPrefix} with preferred prefix
--- 2015-12-03 13:26:10
-
---
--- Table structure for table {$dbPrefix}constants
---
-CREATE TABLE {$dbPrefix}constants (
+<?php
+return [
+"CREATE TABLE {$dbPrefix}constants (
   const_id serial NOT NULL,
   const_name character varying(64) NOT NULL UNIQUE,
   const_value character varying(64) NOT NULL,
   const_immutable smallint NOT NULL DEFAULT 0,
   PRIMARY KEY (const_id)
-);
+)",
 
---
--- Table structure for table {$dbPrefix}groups
---
-
-CREATE TABLE {$dbPrefix}groups (
+"CREATE TABLE {$dbPrefix}groups (
   group_id serial NOT NULL,
   group_name character varying(40) NOT NULL UNIQUE,
   group_description character varying(128) NOT NULL,
   group_auth_level integer NOT NULL DEFAULT 0,
   group_immutable smallint NOT NULL DEFAULT 0,
   PRIMARY KEY (group_id)
-);
+)",
 
---
--- Table structure for table {$dbPrefix}page
---
-
-CREATE TABLE {$dbPrefix}page (
+"CREATE TABLE {$dbPrefix}page (
   page_id serial NOT NULL,
   page_url character varying(255) NOT NULL DEFAULT '/'::character varying,
   page_type character varying(20) NOT NULL DEFAULT 'text/html'::character varying,
@@ -42,13 +29,9 @@ CREATE TABLE {$dbPrefix}page (
   page_immutable smallint NOT NULL DEFAULT 0,
   PRIMARY KEY (page_id),
   UNIQUE (page_url)
-);
+)",
 
---
--- Table structure for table {$dbPrefix}people
---
-
-CREATE TABLE {$dbPrefix}people (
+"CREATE TABLE {$dbPrefix}people (
   people_id serial NOT NULL,
   login_id character varying(60) NOT NULL,
   real_name character varying(50) NOT NULL,
@@ -64,32 +47,24 @@ CREATE TABLE {$dbPrefix}people (
   PRIMARY KEY (people_id),
   UNIQUE (login_id),
   UNIQUE (short_name)
-);
+)",
 
---
--- Table structure for table {$dbPrefix}people_group_map
---
-
-CREATE TABLE {$dbPrefix}people_group_map (
+"CREATE TABLE {$dbPrefix}people_group_map (
   pgm_id serial NOT NULL,
   people_id integer NOT NULL,
   group_id integer NOT NULL DEFAULT '3',
   PRIMARY KEY (pgm_id)
-);
-CREATE INDEX pgm_people_id_idx on {$dbPrefix}people_group_map (people_id);
-CREATE INDEX pgm_group_id_idx on {$dbPrefix}people_group_map (group_id);
-CREATE UNIQUE INDEX people_group_idx on {$dbPrefix}people_group_map (people_id, group_id);
+)",
+"CREATE INDEX pgm_people_id_idx on {$dbPrefix}people_group_map (people_id)",
+"CREATE INDEX pgm_group_id_idx on {$dbPrefix}people_group_map (group_id)",
+"CREATE UNIQUE INDEX people_group_idx on {$dbPrefix}people_group_map (people_id, group_id)",
 
-ALTER TABLE ONLY {$dbPrefix}people_group_map
-    ADD CONSTRAINT {$dbPrefix}pgm_ibfk_1 FOREIGN KEY (people_id) REFERENCES {$dbPrefix}people (people_id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE ONLY {$dbPrefix}people_group_map
-    ADD CONSTRAINT {$dbPrefix}pgm_ibfk_2 FOREIGN KEY (group_id) REFERENCES {$dbPrefix}groups (group_id) ON DELETE CASCADE ON UPDATE CASCADE;
+"ALTER TABLE ONLY {$dbPrefix}people_group_map
+    ADD CONSTRAINT {$dbPrefix}pgm_ibfk_1 FOREIGN KEY (people_id) REFERENCES {$dbPrefix}people (people_id) ON DELETE CASCADE ON UPDATE CASCADE",
+"ALTER TABLE ONLY {$dbPrefix}people_group_map
+    ADD CONSTRAINT {$dbPrefix}pgm_ibfk_2 FOREIGN KEY (group_id) REFERENCES {$dbPrefix}groups (group_id) ON DELETE CASCADE ON UPDATE CASCADE",
 
---
--- Table structure for table {$dbPrefix}routes
---
-
-CREATE TABLE {$dbPrefix}routes (
+"CREATE TABLE {$dbPrefix}routes (
   route_id serial NOT NULL,
   route_path character varying(128) NOT NULL,
   route_class character varying(64) NOT NULL,
@@ -97,32 +72,25 @@ CREATE TABLE {$dbPrefix}routes (
   route_action character varying(255) NOT NULL,
   route_immutable smallint NOT NULL DEFAULT 0::smallint,
   PRIMARY KEY (route_id)
-);
-CREATE UNIQUE INDEX route_path_idx ON {$dbPrefix}routes (route_path);
-ALTER SEQUENCE {$dbPrefix}routes_route_id_seq RESTART WITH 10;
+)",
+"CREATE UNIQUE INDEX route_path_idx ON {$dbPrefix}routes (route_path)",
+"ALTER SEQUENCE {$dbPrefix}routes_route_id_seq RESTART WITH 10",
 
---
--- Table structure for table {$dbPrefix}routes_group_map
---
-CREATE TABLE {$dbPrefix}routes_group_map (
+"CREATE TABLE {$dbPrefix}routes_group_map (
   rgm_id serial NOT NULL,
   route_id integer NOT NULL DEFAULT 0,
   group_id integer NOT NULL DEFAULT 0,
   PRIMARY KEY (rgm_id)
-);
-CREATE INDEX rgm_group_id_idx ON {$dbPrefix}routes_group_map (group_id);
-CREATE INDEX rgm_route_id_idx ON {$dbPrefix}routes_group_map (route_id);
-CREATE UNIQUE INDEX rgm_idx ON {$dbPrefix}routes_group_map (route_id, group_id);
-ALTER TABLE {$dbPrefix}routes_group_map
-    ADD CONSTRAINT {$dbPrefix}routes_group_map_ibfk_1 FOREIGN KEY (route_id) REFERENCES {$dbPrefix}routes (route_id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE {$dbPrefix}routes_group_map
-    ADD CONSTRAINT {$dbPrefix}routes_group_map_ibfk_2 FOREIGN KEY (group_id) REFERENCES {$dbPrefix}groups (group_id) ON DELETE CASCADE ON UPDATE CASCADE;
+)",
+"CREATE INDEX rgm_group_id_idx ON {$dbPrefix}routes_group_map (group_id)",
+"CREATE INDEX rgm_route_id_idx ON {$dbPrefix}routes_group_map (route_id)",
+"CREATE UNIQUE INDEX rgm_idx ON {$dbPrefix}routes_group_map (route_id, group_id)",
+"ALTER TABLE {$dbPrefix}routes_group_map
+    ADD CONSTRAINT {$dbPrefix}routes_group_map_ibfk_1 FOREIGN KEY (route_id) REFERENCES {$dbPrefix}routes (route_id) ON DELETE CASCADE ON UPDATE CASCADE",
+"ALTER TABLE {$dbPrefix}routes_group_map
+    ADD CONSTRAINT {$dbPrefix}routes_group_map_ibfk_2 FOREIGN KEY (group_id) REFERENCES {$dbPrefix}groups (group_id) ON DELETE CASCADE ON UPDATE CASCADE",
 
---
--- Dumping data for tables
---
-
-INSERT INTO {$dbPrefix}constants
+"INSERT INTO {$dbPrefix}constants
     (const_name, const_value, const_immutable)
 VALUES
     ('DISPLAY_DATE_FORMAT','m/d/Y',1),
@@ -145,20 +113,18 @@ VALUES
     ('DISPLAY_PHONE_FORMAT','XXX-XXX-XXXX',1),
     ('SESSION_IDLE_TIME','1800',1),
     ('THEMES_DIR','',1),
-    ('RIGHTS_HOLDER','Reveal IT Consulting',1);
+    ('RIGHTS_HOLDER','Reveal IT Consulting',1)",
 
-
-INSERT INTO {$dbPrefix}groups
+"INSERT INTO {$dbPrefix}groups
     (group_name, group_description, group_auth_level, group_immutable)
 VALUES
     ('SuperAdmin','The group for super administrators. There should be only a couple of these.',10,1),
     ('Managers','Most people accessing the manager should be in this group.',9,1),
-    ('Editor','Editor for the CMS which doesn&#039;t exist in the FtpManager',5,1),
-    ('Registered','The group for people that should&#039;t have access to the manager.',3,1),
-    ('Anonymous','Not logged in, possibly unregistered',0,1);
+    ('Editor','Editor for the CMS',5,1),
+    ('Registered','The group for people that should not have access to the manager.',3,1),
+    ('Anonymous','Not logged in, possibly unregistered',0,1)",
 
-
-INSERT INTO {$dbPrefix}page
+"INSERT INTO {$dbPrefix}page
     (page_url, page_type, page_title, page_description, page_base_url, page_lang, page_charset, page_immutable)
 VALUES
     ('/manager/','text/html','Manager','Manages People, Places and Things','/','en','utf-8',1),
@@ -174,24 +140,24 @@ VALUES
     ('/manager/people/verify/','text/html','Manager for People','Manages people, verifies a person should be deleted.','/','en','utf-8',1),
 	('/manager/routes/','text/html','Manager for Routes','Manages the routes','/','en','utf-8',1),
 	('/manager/routes/verify/','text/html','Manager for Routes','Manages the routes, verifies route should be deleted.','/','en','utf-8',1),
-	('/manager/tests/','text/html','Manager Tests','Runs tests for the code.','/','en','utf-8',1);
+	('/manager/tests/','text/html','Manager Tests','Runs tests for the code.','/','en','utf-8',1)",
 
-INSERT INTO {$dbPrefix}people
+"INSERT INTO {$dbPrefix}people
     (login_id, real_name, short_name, password, description, is_logged_in, bad_login_count, bad_login_ts, is_active, is_immutable, created_on)
 VALUES
     ('SuperAdmin','Super Admin','GSA','$2y$10$mAQZrjwnPDkfpdhmdfqxFuBJwY7w5HeCli2qs2H2Kg69w0MooNsJW','The all powerful Admin',0,0,0,1,1,'2012-08-12 07:55:28'),
-	('Admin','Admin','ADM','$2y$10$mAQZrjwnPDkfpdhmdfqxFuBJwY7w5HeCli2qs2H2Kg69w0MooNsJW','Allowed to admin the backend.',1,0,0,1,1,'2015-09-04 18:15:55');
+	('Admin','Admin','ADM','$2y$10$mAQZrjwnPDkfpdhmdfqxFuBJwY7w5HeCli2qs2H2Kg69w0MooNsJW','Allowed to admin the backend.',1,0,0,1,1,'2015-09-04 18:15:55')",
 
-INSERT INTO {$dbPrefix}people_group_map
+"INSERT INTO {$dbPrefix}people_group_map
     (people_id, group_id)
 VALUES
     (1,1),
 	(1,2),
 	(1,3),
 	(2,2),
-	(2,3);
+	(2,3)",
 
-INSERT INTO {$dbPrefix}routes
+"INSERT INTO {$dbPrefix}routes
     (route_id, route_path, route_class, route_method, route_action, route_immutable)
 VALUES
     (1, '/manager/','ManagerController','render','',1),
@@ -202,9 +168,9 @@ VALUES
 	(6, '/manager/groups/','ManagerController','renderGroupsAdmin','',1),
 	(7, '/manager/pages/','ManagerController','renderPageAdmin','',1),
 	(8, '/manager/tests/','ManagerController','renderTestsAdmin','',1),
-	(9, '/manager/logout/','ManagerController','render','logout',1);
+	(9, '/manager/logout/','ManagerController','render','logout',1)",
 
-INSERT INTO {$dbPrefix}routes_group_map
+"INSERT INTO {$dbPrefix}routes_group_map
     (route_id, group_id)
 VALUES
     (1,1),
@@ -229,7 +195,8 @@ VALUES
 	(9,2),
 	(9,3),
 	(9,4),
-	(9,5);
+	(9,5)"
+];
 
 
 
