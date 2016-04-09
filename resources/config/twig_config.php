9b097c57264b2880d7e8a8947b1442efc494ee68
William E Reveal (from Mac) <bill@revealitconsulting.com>
2016-04-09 18:02:10 -0500

    Refactored twig namespace names to avoid ambiguity

--- resources/config/twig_config.php
+++ resources/config/twig_config.php
@@ -3,12 +3,12 @@ $library_path  = SRC_PATH . '/Ritc/Library/resources/templates';
 return array(
     'default_path'     => $library_path,
     'additional_paths' => array(
-        $library_path . '/tests'    => 'tests',
-        $library_path . '/snippets' => 'snippets',
-        $library_path . '/pages'    => 'pages',
-        $library_path . '/elements' => 'elements',
-        $library_path . '/default'  => 'default',
-        $library_path . '/forms'    => 'forms',
+        $library_path . '/tests'    => 'lib_tests',
+        $library_path . '/snippets' => 'lib_snippets',
+        $library_path . '/pages'    => 'lib_pages',
+        $library_path . '/elements' => 'lib_elements',
+        $library_path . '/default'  => 'lib_default',
+        $library_path . '/forms'    => 'lib_forms',
     ),
     'environment_options' => array(
         'cache'       => APP_PATH . '/twig_cache',
