c9f1c9f3b293b757b5dacc1988423871fe6c2a4d
William E Reveal (from Mac) <bill@revealitconsulting.com>
2016-09-03 12:48:27 -0500

    Modified a trait
    Not gonna make any more changes today so time for a commit.

--- Traits/ControllerTraits.php
+++ Traits/ControllerTraits.php
@@ -5,12 +5,12 @@
  * @file      ControllerTraits.php
  * @namespace Ritc\Library\Traits
  * @author    William E Reveal <bill@revealitconsulting.com>
- * @version   1.0.0-alpha.1
- * @date      2016-04-15 12:45:50
+ * @version   1.0.0
+ * @date      2016-09-03 12:47:21
  * @note Change Log
+ * - v1.0.0         - added one more commonly used property and out of alpha    - 2016-09-03 wer
  * - v1.0.0-alpha.1 - added a couple more commonly used properties and setters  - 2016-04-15 wer
  * - v1.0.0-alpha.0 - Initial version                                           - 2016-04-11 wer
- * @todo ControllerTraits.php - refactor other controllers to use this.
  */
 namespace Ritc\Library\Traits;
 
@@ -45,6 +45,8 @@ trait ControllerTraits
     /** @var  Session */
     protected $o_session;
     /** @var  string */
+    protected $route_method;
+    /** @var  string */
     protected $url_action_one;
 
     protected function setupController(Di $o_di)
@@ -68,6 +70,7 @@ trait ControllerTraits
         $this->a_router_parts = $a_router_parts;
         $this->a_post         = $a_router_parts['post'];
         $this->form_action    = $a_router_parts['form_action'];
+        $this->route_method   = $a_router_parts['route_method'];
         $this->url_action_one = isset($a_router_parts['url_actions'][0])
             ? $a_router_parts['url_actions'][0]
             : '';
