d1013d7ddbede28b698c896a26cb652fcca20e56
William E Reveal (from Mac) <bill@revealitconsulting.com>
2016-10-17 11:34:54 -0500

    Thinking of a way to start using Exceptions to throw errors.

---
+++ Abstracts/CustomException.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @brief     Abstract which extends php Exception class.
+ * @ingroup   lib_interfaces
+ * @file      Ritc/Library/Abstracts/CustomException.php
+ * @namespace Ritc\Library\Abstracts
+ * @author    William E Reveal <bill@revealitconsulting.com>
+ * @version   1.0.0-alpha.0
+ * @date      2016-10-17 11:24:10
+ * @note Change Log
+ * - v1.0.0-alpha.0 - Initial version                                                                   - 2016-10-17 wer
+ */
+namespace Ritc\Library\Abstracts;
+
+use Ritc\Library\Interfaces\CustomExceptionInterface;
+use Ritc\Library\Traits\LogitTraits;
+
+/**
+ * Class CustomException.
+ * @class   CustomException
+ * @package Ritc\Library\Abstracts
+ */
+abstract class CustomException extends \Exception implements CustomExceptionInterface
+{
+    use LogitTraits;
+
+    public function __construct($message = '', $code = 0)
+    {
+        if ($message == '') {
+            throw new $this('No Message Provided for ' . get_class($this));
+        }
+        parent::__construct($message, $code);
+    }
+
+    public function __toString()
+    {
+        return get_class($this) .
+        " '{$this->message}' in {$this->file}.{$this->line}\n" .
+        $this->getTraceAsString();
+    }
+
+}
---
+++ Interfaces/CustomExceptionInterface.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @brief     Interface for Custom Exception Classes.
+ * @ingroup   lib_interfaces
+ * @file      Ritc/Library/Interfaces/CustomExceptionInterface.php
+ * @namespace Ritc\Library\Interfaces
+ * @author    William E Reveal <bill@revealitconsulting.com>
+ * @version   1.0.0-alpha.0
+ * @date      2016-10-17 11:20:07
+ * @note <b>Change Log</b>
+ * - v1.0.0.alpha-0 initial version                                                                     - 2016-10-17 wer
+ */
+namespace Ritc\Library\Interfaces;
+
+/**
+ * Interface CustomExceptionInterface
+ * @class CustomExceptionInterface
+ * @package Ritc\Library\Interfaces
+ */
+interface CustomExceptionInterface
+{
+    /* Protected methods inherited from Exception class */
+    public function getMessage();                 // Exception message
+    public function getCode();                    // User-defined Exception code
+    public function getFile();                    // Source filename
+    public function getLine();                    // Source line
+    public function getTrace();                   // An array of the backtrace()
+    public function getTraceAsString();           // Formated string of trace
+
+    /* Overrides methods inherited from Exception class */
+    public function __toString();                 // formated string for display
+    public function __construct($message = null, $code = 0);}
