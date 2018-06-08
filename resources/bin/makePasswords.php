7cdbb2cd3441e175a5f7cfe9ee39eb91e4cf51ef
William E Reveal (from Mac) <bill@revealitconsulting.com>
2018-06-08 10:44:03 -0500

    Removed obsolete bin files, updated one

--- resources/bin/makePasswords.php
+++ resources/bin/makePasswords.php
@@ -1,4 +1,10 @@
 <?php
-print 'SuperAdmin: ' . password_hash('letGSAin', PASSWORD_DEFAULT) . "\n";
-print 'Admin: ' . password_hash('letADMin', PASSWORD_DEFAULT) . "\n";
+if (isset($argv[1])) {
+    print 'The Password: ' . password_hash($argv[1], PASSWORD_DEFAULT) . "\n";
+}
+else {
+    print 'SuperAdmin: ' . password_hash('letGSAin', PASSWORD_DEFAULT) . "\n";
+    print 'Admin: ' . password_hash('letADMin', PASSWORD_DEFAULT) . "\n";
+    print 'Manager: ' . password_hash('letMANin', PASSWORD_DEFAULT) . "\n";
+}
 ?>
