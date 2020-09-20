e9c318161fcae6779c4727fa5726c83d49356845
William E Reveal (from Mac) <bill@revealitconsulting.com>
2020-09-20 17:06:06 -0500

    Updated the CacheFactory to match changes in Symfony/Cache component

--- Factories/CacheFactory.php
+++ Factories/CacheFactory.php
@@ -5,33 +5,32 @@
  */
 namespace Ritc\Library\Factories;
 
+use DomainException;
+use PDO;
+use PDOException;
 use Ritc\Library\Traits\LogitTraits;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\Cache\Adapter\ChainAdapter;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\Adapter\FilesystemTagAwareAdapter;
 use Symfony\Component\Cache\Adapter\PdoAdapter;
 use Symfony\Component\Cache\Adapter\PhpArrayAdapter;
 use Symfony\Component\Cache\Adapter\PhpFilesAdapter;
 use Symfony\Component\Cache\Adapter\RedisAdapter;
+use Symfony\Component\Cache\Adapter\RedisTagAwareAdapter;
 use Symfony\Component\Cache\Adapter\TagAwareAdapter;
 use Symfony\Component\Cache\Exception\CacheException;
-use Symfony\Component\Cache\Simple\ArrayCache;
-use Symfony\Component\Cache\Simple\ChainCache;
-use Symfony\Component\Cache\Simple\FilesystemCache;
-use Symfony\Component\Cache\Simple\PdoCache;
-use Symfony\Component\Cache\Simple\PhpArrayCache;
-use Symfony\Component\Cache\Simple\PhpFilesCache;
-use Symfony\Component\Cache\Simple\Psr6Cache;
-use Symfony\Component\Cache\Simple\RedisCache;
+use Symfony\Component\Cache\Exception\InvalidArgumentException;
 
 /**
  * Class CacheFactory - creates one of several different Symfony\Component\Cache objects.
  *
  * @author  William E Reveal <bill@revealitconsulting.com>
- * @version v1.0.0-alpha.0
- * @date    2018-05-12 21:42:20
+ * @version v1.0.0-alpha.2
+ * @date    2020-09-20 15:55:27
  * @change_log
- * - v1.0.0-alpha.0 - Initial version        - 2018-05-12 wer
+ * - v1.0.0-alpha.2 - Changed to latest version of Symfony\Component\Cache  - 2020-09-20 wer
+ * - v1.0.0-alpha.0 - Initial version                                       - 2018-05-12 wer
  */
 class CacheFactory
 {
@@ -39,7 +38,7 @@ class CacheFactory
 
     /** @var array  */
     private static $instance = [];
-    /** @var ArrayAdapter|ChainAdapter|FilesystemAdapter|PdoAdapter|PhpArrayAdapter|PhpFilesAdapter|RedisAdapter|ArrayCache|ChainCache|FilesystemCache|PdoCache|PhpArrayCache|PhpFilesCache|Psr6Cache|RedisCache  */
+    /** @var ArrayAdapter|ChainAdapter|FilesystemAdapter|FilesystemTagAwareAdapter|PdoAdapter|PhpArrayAdapter|PhpFilesAdapter|RedisAdapter  */
     private $o_cache;
 
     /**
@@ -76,53 +75,49 @@ class CacheFactory
             case 'Filesystem':
                 $o_cache = new FilesystemAdapter($namespace, $lifetime, $directory);
                 break;
+            case 'FilesystemTagAware':
+                $o_cache = new FilesystemTagAwareAdapter($namespace,$lifetime, $directory);
+                break;
             case 'Pdo':
-                $o_cache = new PdoAdapter($a_cache_config['connOrDns'], $namespace, $lifetime, $a_cache_config['options']);
+                if ($a_cache_config['connOrDns'] instanceof PDO) {
+                    try {
+                        $o_cache = new PdoAdapter($a_cache_config['connOrDns'], $namespace, $lifetime, $a_cache_config['options']);
+                        try {
+                            $o_cache->createTable();
+                        }
+                        catch(PDOException $e) {
+                            // table must exist don't do anything
+                        }
+                        catch(DomainException $e) {
+                            // something not set right
+                            $o_cache = null;
+                        }
+                    }
+                    catch (InvalidArgumentException $e) {
+                        $o_cache = null;
+                    }
+                }
+                else {
+                    $o_cache = null;
+                }
                 break;
             case 'PhpArray':
                 $o_cache = new PhpArrayAdapter($a_cache_config['file'], $a_cache_config['fallback_pool']);
                 break;
+            case 'Redis':
+                $o_cache = new RedisAdapter($a_cache_config['redis_client'], $namespace, $lifetime);
+                break;
+            case 'RedisTagAware':
+                $redis_client = RedisAdapter::createConnection($a_cache_config['redis_client']);
+                $o_cache = new RedisTagAwareAdapter($redis_client);
+                break;
             case 'PhpFiles':
+            default:
                 try {
                     $o_cache = new PhpFilesAdapter($namespace, $lifetime, $directory);
                 }
                 catch (CacheException $e) {
-                    /** @noinspection ForgottenDebugOutputInspection */
-                    error_log('Unable to create PhpFilesAdapter instance: ' . $e->getMessage());
-                }
-                break;
-            case 'Redis':
-                $o_cache = new RedisAdapter($a_cache_config['redis_client'], $namespace, $lifetime);
-                break;
-            case 'SimpleArray':
-                $o_cache = new ArrayCache($lifetime, $a_cache_config['store_serialized']);
-                break;
-            case 'SimpleChain':
-                $o_cache = new ChainCache($a_cache_config['caches'], $lifetime);
-                break;
-            case 'SimpleFilesystem':
-                $o_cache = new FilesystemCache($namespace, $lifetime, $directory);
-                break;
-            case 'SimplePdo':
-                $o_cache = new PdoCache($a_cache_config['connOrDns'], $namespace, $lifetime, $a_cache_config['options']);
-                break;
-            case 'SimplePhpArray':
-                $o_cache = new PhpArrayCache($a_cache_config['file'], $a_cache_config['fallback_pool']);
-                break;
-            case 'SimplePsr6':
-                $o_cache = new Psr6Cache($a_cache_config['pool_interface']);
-                break;
-            case 'SimpleRedis':
-                $o_cache = new RedisCache($a_cache_config['redis_client'], $namespace, $lifetime);
-                break;
-            case 'SimplePhpFiles':
-            default:
-                try {
-                    $o_cache = new PhpFilesCache($namespace, $lifetime, $directory);
-                }
-                catch (CacheException $e) {
-                    /** @noinspection ForgottenDebugOutputInspection */
-                    error_log('Could not create the PhpFilesCache instance: ' . $e->getMessage());
+                    $o_cache = null;
                 }
         }
         if ($psr6 && is_object($o_cache)) {
@@ -131,7 +126,6 @@ class CacheFactory
         /** @var ArrayAdapter|ChainAdapter|FilesystemAdapter|PdoAdapter|PhpArrayAdapter|PhpFilesAdapter|RedisAdapter
          *       $this->o_cache
          */
-        /** @noinspection UnusedConstructorDependenciesInspection */
         $this->o_cache = $o_cache;
     }
 
